970364f6e97a275b44274dd79f1e43c1c1c0263a
Bryan <bryan.garcia.cuevas@gmail.com>
2024-03-20 13:32:28 -0700

    added banning and unbanning powers to admin

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Admin = require('../model/admin');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const User = require('../model/user');
 
 // register admin
 router.post("/register", async (req, res) => {
@@ -50,6 +51,70 @@ router.post('/login', async (req, res) => {
         return res.json({status: 'error', admin: false})
     }
 
+})
+
+router.put('/ban/:userId', async (req, res) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (token == null) return res.sendStatus(401);
+
+    try {
+        const decoded = jwt.verify(token, 'secret123');
+        const admin = await Admin.findOne({ email: decoded.email });
+
+        if (!admin) {
+            return res.json({ status: 'error', error: 'Not a authorized Admin' });
+        }
+
+        const user = await User.findOne({ _id : req.params.userId})
+
+        console.log(user)
+
+        if (!user) {
+            return res.json({ status: 'error', error: 'User Not Found'})
+        }
+
+        user.status = 'banned'
+        await user.save()
+
+        return res.json({ status: 'ok', user})
+
+    } catch (error) {
+        return res.json({status: 'error', error: error})
+    }
+})
+
+router.put('/unban/:userId', async (req, res) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (token == null) return res.sendStatus(401);
+
+    try {
+        const decoded = jwt.verify(token, 'secret123');
+        const admin = await Admin.findOne({ email: decoded.email });
+
+        if (!admin) {
+            return res.json({ status: 'error', error: 'Not a authorized Admin' });
+        }
+
+        const user = await User.findOne({ _id : req.params.userId})
+
+        console.log(user)
+
+        if (!user) {
+            return res.json({ status: 'error', error: 'User Not Found'})
+        }
+
+        user.status = 'unbanned'
+        await user.save()
+
+        return res.json({ status: 'ok', user})
+
+    } catch (error) {
+        return res.json({status: 'error', error: error})
+    }
 })
 
 module.exports = router;
