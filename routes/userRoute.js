4dd3c93722f9967e14158c5d74afcebcf24bed91
Bryan <bryan.garcia.cuevas@gmail.com>
2024-03-20 15:52:27 -0700

    Add comment deletion functionality

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -160,6 +160,7 @@ router.delete('/post/:id', async (req, res) => {
         }
   
         await Post.deleteOne({_id: req.params.id})
+        await Comment.deleteMany({ postId: req.params.id })
 
         return res.json({ status: 'Ok', post });
     } catch (error) {
@@ -220,7 +221,8 @@ router.post('/comment', async (req, res) => {
             body: req.body.body
         });
 
-        await comment.save();
+        comment.save()
+
         return res.json({ status: 'Ok', comment });
     } catch (error) {
         console.error(error);
@@ -230,5 +232,49 @@ router.post('/comment', async (req, res) => {
         return res.status(500).json({ status: 'error', error: 'Server error during token verification' });
     }
 });
+
+// comment on a specific post from user (only for authorized user)
+router.delete('/comment/:id', async (req, res) => {
+    const authHeader = req.headers['authorization'];
+    if (!authHeader) return res.status(401).json({ status: 'error', error: 'Authorization header missing' });
+
+    const token = authHeader.split(' ')[1];
+    if (!token) return res.status(401).json({ status: 'error', error: 'Bearer token missing' });
+
+    try {
+        const decoded = jwt.verify(token, 'secret123');
+        const user = await User.findOne({ email: decoded.email });
+
+        if (!user) {
+            return res.status(404).json({ status: 'error', error: 'User not found' });
+        }
+
+        if (user.status == 'banned') {
+            return res.json({ status: 'error', error: 'User is banned' })
+        }
+
+        const comment = await Comment.findOne({_id: req.params.id})
+        
+        if (!comment){
+            return res.json({ status: 'error', error: 'Comment not found' });
+        }
+        
+        if (comment.userId.toString() !== user._id.toString()) {
+            return res.json({ status: 'error', error: 'Not a authorized User' });
+        }
+        
+        await Comment.deleteOne({_id: req.params.id})
+
+        return res.json({ status: 'Ok', message: `comment ${req.params.id} deleted` });
+    } catch (error) {
+        console.error(error);
+        if (error instanceof jwt.JsonWebTokenError) {
+            return res.status(403).json({ status: 'error', error: 'Invalid Token' });
+        }
+        return res.status(500).json({ status: 'error', error: error });
+    }
+});
+
+
 
 module.exports = router;
