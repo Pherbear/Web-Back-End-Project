6de81193f5ffa0217e9fa4274609b9ddedbbbf6c
Bryan <bryan.garcia.cuevas@gmail.com>
2024-03-21 14:48:38 -0700

    retrieving posts, and comments gives username

--- routes/viewerRoute.js
+++ routes/viewerRoute.js
@@ -2,12 +2,27 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../model/post');
 const Comment = require('../model/comment');
+const User = require('../model/user');
 
 // Get all posts
 router.get("/posts", async (req, res) => {
     try {
         const posts = await Post.find();
-        res.status(200).json({status: 'Ok', data: posts});
+        const map = new Map()
+        let new_posts = []
+        let index = 0
+        for (let post of posts) {
+            let username
+            if (map.has(post.userId)) {
+                username = map.get(post.userId)
+            } else {
+                const user = await User.findOne({_id: post.userId})
+                username = user.name
+                map.set(post.userId, username)
+            }
+            new_posts.push({...post._doc, name: username})
+        }
+        res.status(200).json({status: 'Ok', data: new_posts});
     } catch (err) {
         res.status(500).json({status: 'error', error: 'Server error'});
     }
@@ -18,11 +33,30 @@ router.get("/posts/:postId", async (req, res) => {
     const { postId } = req.params;
     try {
         const post = await Post.findById(postId);
+
         if (!post) {
             return res.status(404).json({status: 'error', error: 'Post not found'});
         }
+
+        const user = await User.findOne({_id: post.userId})
+        const username = user.name
+        const new_post = {...post._doc, name: username}
+
         const comments = await Comment.find({postId: postId});
-        res.status(200).json({status: 'Ok', data: {post, comments}});
+        let new_comments = []
+        let username_map = new Map()
+        for (const comment of comments) {
+            let comment_username
+            if (username_map.has(comment.userId)) {
+                comment_username = username_map.get(comment.userId)
+            } else {
+                const comment_user = await User.findOne({_id: comment.userId})
+                comment_username = comment_user.name
+                username_map.set(comment.userId, comment_username)
+            }
+            new_comments.push({...comment._doc, name: comment_username})
+        }
+        res.status(200).json({status: 'Ok', data: {post: new_post, comments: new_comments}});
     } catch (err) {
         res.status(500).json({status: 'error', error: 'Server error'});
     }
